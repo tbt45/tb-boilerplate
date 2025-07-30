93c0a7712e9169e84d0372346b9d719fdb38392b
tbt45 <katsu5843_0717@yahoo.co.jp>
2025-07-31 00:56:08 +0900

    ChakraProviderのエラー解消

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<App />);
+// デフォルトテーマでOK
+// const theme = extendTheme({});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChakraProvider value={defaultSystem}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
