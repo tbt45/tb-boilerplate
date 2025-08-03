6b81ca1df125bb31e88ab1e72e5a763997a4c52f
tbt45 <katsu5843_0717@yahoo.co.jp>
2025-08-03 20:42:00 +0900

    サイドバーのui調整

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,6 @@
-// App.tsx
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Box, Flex, Link as ChakraLink } from "@chakra-ui/react";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -14,14 +9,24 @@ import NotFound from "./pages/NotFound";
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Box p={4} bg="gray.100">
+        <Flex gap={4}>
+          <ChakraLink as={Link} to="/">
+            ホーム
+          </ChakraLink>
+          <ChakraLink as={Link} to="/about">
+            このアプリについて
+          </ChakraLink>
+        </Flex>
+      </Box>
+
+      <Box p={4}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Box>
     </Router>
   );
 }
